d4668bbc03807cb91dca0457ef7d405dbb9f2e87
Simon Kojima <simon.kojima@outlook.com>
2022-09-20 17:22:05 +0200

    initial commit of 0.1.7

--- src/pyscab/StimulationController.py
+++ src/pyscab/StimulationController.py
@@ -12,7 +12,7 @@ def get_required_time(plans, data):
 
 class StimulationController(object):
 
-    def __init__(self, AudioHardwareController, marker_send, mode='serial', time_tick = 0.0001, status=0):
+    def __init__(self, AudioHardwareController, marker_send, mode='serial', time_tick = 0.0001, share=0):
         """
         class for playing stimulating plan.
 
@@ -26,18 +26,23 @@ class StimulationController(object):
         time_tick : float, default=0.0001
             pause between each loop in play() function. In terms of real time, it should be reduced.
             However, it also caused intence compulational load.
-        status : int or instance of multiprocessing.Value class
+        share : int or instance of multiprocessing[0] class
             0 : stopped
             1 : running
-            When mode is set to 'serial', type of status is Boolean.
-            When mode is set to 'pararell', type of status is an instance of multiprocessing.Value class.
-            When it's working in pararell mode, it will be stop playing immediate after status is changed to 0.
+            When mode is set to 'serial', type of share is Boolean.
+            When mode is set to 'pararell', type of share is an instance of multiprocessing.Array class.
+
+            share[0] is used for sharing runnin status.
+            When it's working in pararell mode, it will be stop playing immediate after share is changed to 0.
             e.g., when you are presenting stimulus in online BCI application with dynamic stopping,
-            you need to stop playing when it's triggered. In that case, you need to set mode to pararell and use status variable.
+            you need to stop playing when it's triggered. In that case, you need to set mode to pararell and use share variable.
+
+            share[1] is used for sharing current marker value.
+
         """
         self.ahc = AudioHardwareController
         self.time_tick = time_tick
-        self.status = status
+        self.share = share
         self.marker_send = marker_send
         self.mode = mode.lower()
         logger.debug("time_tick for Stimulation Controller was set to %s", str(self.time_tick))
@@ -53,7 +58,7 @@ class StimulationController(object):
     def play_parallel(self, plans, data, time_termination, pause):
         """
         pararell version of play function.
-        type of variable 'status' is instance multiprocessing.Value.
+        type of variable 'share' is instance of multiprocessing.Array
         """
         # pause : pause after playing all sounds in plans.
 
@@ -68,7 +73,7 @@ class StimulationController(object):
         
         logger.debug("session time was set to %s." ,str(time_termination))
 
-        self.status.value = 1
+        self.share[0] = 1
         self.ahc.open()
         logger.debug("Audio Hardware Controller Opening.")
 
@@ -77,12 +82,13 @@ class StimulationController(object):
         time.sleep(1)        
 
         start = self.ahc.get_time_info()['current_time']
-        while self.status.value == 1:
+        while self.share[0] == 1:
             now = self.ahc.get_time_info()['current_time'] - start
             for idx, plan in enumerate(plans):
                 if now > plan[0]:
                     self.ahc.play(data.get_data_by_id(plan[1]),plan[2])
                     self.marker_send(val=plan[3])
+                    self.share[1] = plan[3]
                     del_idxs.append(int(idx))
                     logger.debug("Playing, id:%s, ch:%s, marker:%s, path:%s",str(plan[1]),str(plan[2]),str(plan[3]),data.get_path_by_id(plan[1]))
                 for del_idx in del_idxs:
@@ -90,7 +96,7 @@ class StimulationController(object):
                 del_idxs=list()
             time.sleep(self.time_tick)
             if now > time_termination:
-                self.status.value = 0
+                self.share[0] = 0
 
         time.sleep(pause)
         self.ahc.close()
@@ -99,9 +105,8 @@ class StimulationController(object):
     def play_serial(self, plans, data, time_termination, pause):
         """
         serialized version of play function.
-        type of variable 'status' is int.
+        type of variable 'share' is int.
         """
-
 
         # pause : pause after playing all sounds in plans.
 
@@ -116,7 +121,7 @@ class StimulationController(object):
         
         logger.debug("session time was set to %s." ,str(time_termination))
 
-        self.status = 1
+        self.share = 1
         self.ahc.open()
         logger.debug("Audio Hardware Controller Opening.")
 
@@ -125,7 +130,7 @@ class StimulationController(object):
         time.sleep(1)        
 
         start = self.ahc.get_time_info()['current_time']
-        while self.status == 1:
+        while self.share == 1:
             now = self.ahc.get_time_info()['current_time'] - start
             for idx, plan in enumerate(plans):
                 if now > plan[0]:
@@ -138,7 +143,7 @@ class StimulationController(object):
                 del_idxs=list()
             time.sleep(self.time_tick)
             if now > time_termination:
-                self.status = 0
+                self.share = 0
 
         time.sleep(pause)
         self.ahc.close()
